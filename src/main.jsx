3ab53f5fa275718aad238df0b7d7a230b6635991
Nikunj Agarwal <180705nikunj@gmail.com>
2025-01-20 10:25:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
-const router = createBrowserRouter([]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
